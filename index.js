458f9d53e9c0b147e5057cab2c1e88ee3b11c069
karlijones <karlijones1984@gmail.com>
2024-09-15 12:21:33 -0500

    helper function to run async/await

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 const Runner = require('./runner');
 const runner = new Runner();
+
+// helper function to run async/await
+const run = async () => {
+    const results = await runner.collectFiles();
+    console.log(results);
+};
+
+run();
