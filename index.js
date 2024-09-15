f9ff7848629567beac2463108098751c1a0f3f05
karlijones <karlijones1984@gmail.com>
2024-09-15 12:23:16 -0500

    process cwd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const runner = new Runner();
 
 // helper function to run async/await
 const run = async () => {
-    const results = await runner.collectFiles();
+    const results = await runner.collectFiles(process.cwd());
     console.log(results);
 };
 
