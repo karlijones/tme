0a29ab00dc08b4f17cb40b7ebfdf9232b21e9320
karlijones <karlijones1984@gmail.com>
2024-09-15 16:36:04 -0500

    additions to render function

--- render.js
+++ render.js
@@ -2,6 +2,15 @@ const path = require('path');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const render = (filename) => {
+const render = async (filename) => {
+    const filePath = path.join(process.cwd(), filename);
 
+    const dom = await JSDOM.fromFile(filePath, {
+        runScripts: 'dangerously',
+        resources: 'usable'
+    });
+
+    return dom;
 };
+
+module.exports = render;
