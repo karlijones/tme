c31b26c23630a530096e387f2664586b31f6be23
karlijones <karlijones1984@gmail.com>
2024-09-15 15:27:16 -0500

    forbiddenDirs for directories we want to ignore

--- runner.js
+++ runner.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
+const forbiddenDirs = ['node_modules'];
 class Runner {
     constructor() {
         this.testFiles = [];
