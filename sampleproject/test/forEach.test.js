9c4ead21f70e56c9447613e347b7a09067bc03e1
karlijones <karlijones1984@gmail.com>
2024-09-15 14:23:36 -0500

    beforeEach

--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -1,10 +1,13 @@
 const assert = require('assert');
 const { forEach } = require('../index');
 
-it('should sum an array', () => {
-    const numbers = [1,2,3];
+let numbers;
+beforeEach(() => {
+    numbers = [1,2,3];
+});
 
-    let total = 0;
+it('should sum an array', () => {
+   let total = 0;
     forEach(numbers, (value) => {
         total += value;
     });
